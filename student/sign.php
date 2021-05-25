d947741ed8bccd179644d768ecb7bd2549e9fe98
xig1517 <xig1517@gmail.com>
2021-05-25 15:57:14 +0800

    ver1

---
+++ student/sign.php
@@ -0,0 +1,17 @@
+<?php
+$ERRMSG = "";
+
+//require '../apis/connect.php';
+require '../apis/class_time.php';
+
+session_start();
+session_destroy();
+
+//if(isset($_POST['SIGNIN'])){
+//    $sql = "INSERT INTO class_check_io (class, date, check_in, check_out, id) VALUES ()";
+//}
+
+echo classInTimeHour[1];
+
+
+?>
